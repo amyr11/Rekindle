31ba3b5a87e85198b3cfabaec922957c48b20607
amyrfrancisco <amyrdrawings@gmail.com>
2021-08-24 01:15:25 +0800

    set night mode on StartActivity.java

--- app/src/main/java/com/runtimeterror/rekindle/StartActivity.java
+++ app/src/main/java/com/runtimeterror/rekindle/StartActivity.java
@@ -1,6 +1,7 @@
 package com.runtimeterror.rekindle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.os.Bundle;
 
@@ -10,5 +11,6 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
     }
 }
