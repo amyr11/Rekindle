4cfbea7202b742d225282663949af13a14f0404c
Luwesss <siuolpilihp@gmail.com>
2021-08-25 19:59:53 +0800

    Created Join Existing Thread Activity

---
+++ app/src/main/java/com/runtimeterror/rekindle/JoinExistingThread.java
@@ -0,0 +1,14 @@
+package com.runtimeterror.rekindle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class JoinExistingThread extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join_existing_thread);
+    }
+}
