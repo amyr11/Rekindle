1454ad0f1f53235492f213df7dc7449a2bea4f8c
Luwesss <siuolpilihp@gmail.com>
2021-08-25 20:23:24 +0800

    Created CopyThreadCode activity

---
+++ app/src/main/java/com/runtimeterror/rekindle/CopyThreadCode.java
@@ -0,0 +1,14 @@
+package com.runtimeterror.rekindle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CopyThreadCode extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_copy_thread_code);
+    }
+}
