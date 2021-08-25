8f523befd419b10c91869dd787999dc7611340e0
Luwesss <siuolpilihp@gmail.com>
2021-08-25 19:21:37 +0800

    Added Create new Thread Activity

---
+++ app/src/main/java/com/runtimeterror/rekindle/CreateThread.java
@@ -0,0 +1,14 @@
+package com.runtimeterror.rekindle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateThread extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_thread);
+    }
+}
