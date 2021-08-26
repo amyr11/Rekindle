493b24d359610795765237fa820dfe3f586e1c4a
Luwesss <siuolpilihp@gmail.com>
2021-08-26 20:14:19 +0800

    Added FireStore SDK

--- app/src/main/java/com/runtimeterror/rekindle/firebase/MassagingService.java
+++ app/src/main/java/com/runtimeterror/rekindle/firebase/MassagingService.java
@@ -1,7 +1,5 @@
 package com.runtimeterror.rekindle.firebase;
 
-import android.util.Log;
-
 import androidx.annotation.NonNull;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
@@ -12,12 +10,10 @@ public class MassagingService extends FirebaseMessagingService{
     @Override
     public void onNewToken(@NonNull String token) {
         super.onNewToken(token);
-        Log.d("FCM","Token: " + token);
     }
 
     @Override
     public void onMessageReceived(@NonNull RemoteMessage remoteMessage){
         super.onMessageReceived(remoteMessage);
-        Log.d("FCM","Message: " + remoteMessage.getNotification().getBody());
     }
 }
