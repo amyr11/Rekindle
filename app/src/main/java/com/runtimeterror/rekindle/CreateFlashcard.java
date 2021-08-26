4272d012a025fd234b4d4919cbbaa6564bf20bd0
amyrfrancisco <amyrdrawings@gmail.com>
2021-08-26 13:47:26 +0800

    fixed activity_create_flashcard_thread.xml

--- app/src/main/java/com/runtimeterror/rekindle/CreateFlashcard.java
+++ app/src/main/java/com/runtimeterror/rekindle/CreateFlashcard.java
@@ -9,6 +9,6 @@ public class CreateFlashcard extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_flashcard);
+        setContentView(R.layout.activity_create_flashcard_thread);
     }
 }
--- app/src/main/java/com/runtimeterror/rekindle/PersonalFlashcardsActivity.java
+++ app/src/main/java/com/runtimeterror/rekindle/PersonalFlashcardsActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
